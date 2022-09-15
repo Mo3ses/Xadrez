ea187226892028fe2788e7f5c187801b0af169d1
Moises <mo3ses404@gmail.com>
2022-09-14 22:22:28 -0300

    classe posicao criada

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Tabuleiro;
+class Program {
+    static void Main(String[] args){
+            Posicao P = new Posicao(3,4);
+
+            System.Console.WriteLine($"Posição: {P}");
+    }
+}
